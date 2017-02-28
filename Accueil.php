f2c1a176eae2eb652112dc9064fd33f2e1631fce
sneakyPeat <david.ducluzeaud@live.fr>
2017-02-28 16:03:59 +0100

    Fin moteur de recherche

--- Accueil.php
+++ Accueil.php
@@ -23,77 +23,86 @@ session_start();
 
   <br><br>
 
-
+  <section class="recherche">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-offset-2">
+          <nav class="navbar-form navbar-collapse">
+            <div class="form-group">
+              <form action="" method="Post">
+                <input type="search" class="input-lg form-control" placeholder="Rechercher un CV" name="requete">
+                <button type="submit" class="btn-lg btn-primary btn"><span class="glyphicon glyphicon-eye-open"></span> Rechercher</button>
+              </form>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </section>
 
   <?php
 
   if (isset($_POST['requete']) && $_POST['requete'] != null) {
-      include('connexion_BDD.php');
+    include('connexion_BDD.php');
 
-      $requete = htmlspecialchars($_POST['requete']);//La fonction extract puet t'aidér pour sa
-    $req = $bdd->prepare("SELECT * FROM membres WHERE nom LIKE :requete ORDER BY id DESC"); // la requête, que vous devez maintenant comprendre
+    $requete = htmlspecialchars($_POST['requete']);
+    $req = $bdd->prepare("SELECT nom, prenom
+                          FROM etudiant
+                          WHERE ID_etu IN (SELECT ID_etu
+                                            FROM formations
+                                             WHERE intitule_form1 LIKE :requete
+                                                or intitule_form2 Like :requete
+                                                or intitule_form3 LIKE :requete
+                                                ORDER BY id DESC)");
+
     $req->execute(array('requete' => $requete . '%'));
 
 
-      $nb_resultats = $req->rowCount(); // on utilise la fonction mysql_num_rows pour compter les résultats pour vérifier par la suite
+    $nb_resultats = $req->rowCount();
     if ($nb_resultats != 0) { // si le nombre de résultats est supérieur à 0, on continue
-
       ?>
 
       <br>
-      <div class="error">
 
-
-        <p>Nous avons trouvé <?php echo $nb_resultats;
-
-        if ($nb_resultats > 1) {
-            echo ' résultats';
-        } else {
-            echo ' résultat';
-        } // on vérifie le nombre de résultats correctement.
-        ?>
-        dans notre base de données. <br/>
-        <br/>
-      </div>
       <div class="container">
-        <div class="row">
 
-            <div class="form-group">
-              <div class="col-md-offset-4">
-              <div class="row">
-                <div class="col-md-7">
-                  <table class="table table table-striped">
-                      <thead class="form-group">
-                        <tr>
-                          <th>Nom</th>
-                          <th>Prénom</th>
-                          <th>CV</th>
-                          <th>Email</th>
-                          <th>Télecharger</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <?php
-                        while ($donnees = $req->fetch()) { // on fait un while pour afficher la liste des fonctions trouvées, ainsi que l'id qui permettra de faire le lien vers la page de la fonction
-                          ?>
-                          <tr>
-                            <td><?php echo $donnees['nom']; ?></td>
-                            <td><?php  echo $donnees['prenom']; ?></td>
-                            <td><a href="#" class="btn btn-primary btn-large"><span class="glyphicon glyphicon-picture"></span></a></td>
-                            <td><a href="#" class="btn btn-info btn-large"><span class="glyphicon glyphicon-send"></span> </a></td>
-                            <td><a href="#" class="btn btn-success btn-large"><span class="glyphicon glyphicon-save"></span> </a></td>
 
-                          </tr>
-                          <?php
+        <div class="form-group">
+          <div class="col-md-offset-3">
+            <div class="row">
+              <div class="col-md-8">
+                <table class="table table table-striped">
+                  <thead class="form-group">
+                    <tr>
+                      <th>Nom</th>
+                      <th>Prénom</th>
+                      <th>Email</th>
+                      <th>CV</th>
+                      <th>Télecharger</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    while ($donnees = $req->fetch()) { // on fait un while pour afficher la liste des fonctions trouvées, ainsi que l'id qui permettra de faire le lien vers la page de la fonction
+                      ?>
+                      <tr>
+                        <td><?php echo $donnees['nom']; ?></td>
+                        <td><?php  echo $donnees['prenom']; ?></td>
+                        <td><?php  echo $donnees['mail']; ?></td>
+                        <td><a href="#" class="btn btn-primary btn-large"><span class="glyphicon glyphicon-picture"></span></a></td>
+                        <td><a href="#" class="btn btn-success btn-large"><span class="glyphicon glyphicon-save"></span> </a></td>
 
-                        } // fin de la boucle
-                        ?>
+                      </tr>
+                      <?php
 
-                      </tbody>
-                    </table>
-                  </div>
+                    } // fin de la boucle
+                    ?>
+
+                  </tbody>
+                </table>
               </div>
             </div>
+
           </div>
         </div>
       </div>
@@ -101,32 +110,13 @@ session_start();
 
     } // Fin d'affichage des résultats
     else {
-        ?>
+      ?>
       <h3>Pas de résultats</h3>
       <p>Nous n'avons trouvé aucun résultat pour votre requête "<?php echo $_POST['requete']; ?>". Réessayez</a> avec autre chose.</p>
       <?php
 
     }// fin de l'affichage des erreurs
     $req->closeCursor(); // on ferme mysql
-  } else {
-?>
-    <section class="recherche">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-offset-2">
-            <nav class="navbar-form navbar-collapse">
-              <div class="form-group">
-                <form action="" method="Post">
-                  <input type="search" class="input-lg form-control" placeholder="Rechercher un CV" name="requete">
-                  <button type="submit" class="btn-lg btn-primary btn"><span class="glyphicon glyphicon-eye-open"></span> Rechercher</button>
-                </form>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </section>
-    <?
   }
   ?>
 
--- connexion_BDD.php
+++ connexion_BDD.php
@@ -2,7 +2,7 @@
 
 try
 {
-  $bdd = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8', 'root', 'root');
+  $bdd = new PDO('mysql:host=localhost;dbname=CV;charset=utf8', 'root', 'root');
 }
 catch(Exception $e)
 {
