5ea9f5aec81cc7c3353f1d951bbe56aeebef4bfe
sneakyPeat <david.ducluzeaud@live.fr>
2017-03-05 18:59:04 +0100

    FIN
    Il faudrait que tu t’ajoutes dans la base ce serait pas mal

--- BDD_CV.sql
+++ BDD_CV.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:8889
--- Généré le :  Sam 04 Mars 2017 à 14:07
+-- Généré le :  Dim 05 Mars 2017 à 18:58
 -- Version du serveur :  5.6.34
 -- Version de PHP :  7.1.0
 
@@ -31,16 +31,7 @@ CREATE TABLE `associatif` (
 --
 
 INSERT INTO `associatif` (`ID`, `ID_etu`, `association`) VALUES
-(89, 8, 'vzd'),
-(90, 9, 'lze lez c eldne ccefne clec'),
-(91, 10, 'lnlkazlkal,'),
-(103, 11, 'clz  mka dampm '),
-(104, 13, 'zefzf'),
-(105, 14, 'zefea azds za '),
-(111, 15, ''),
-(112, 16, 'messe'),
-(119, 17, 'f&quot;kjnfk&quot;f'),
-(120, 18, '');
+(126, 22, '');
 
 -- --------------------------------------------------------
 
@@ -61,16 +52,7 @@ CREATE TABLE `competences` (
 --
 
 INSERT INTO `competences` (`ID`, `ID_etu`, `langues1`, `langues2`, `langues3`) VALUES
-(39, 8, 'anglaise', '', ''),
-(40, 9, 'anglais', '', ''),
-(41, 10, 'allemand', '', ''),
-(53, 11, 'italien', '', ''),
-(54, 13, 'français', '', ''),
-(55, 14, 'anglais', '', ''),
-(61, 15, 'efface fez fr re ', '', ''),
-(62, 16, 'français', '', ''),
-(69, 17, 'allemand', '', ''),
-(70, 18, 'chinois', '', '');
+(76, 22, 'anglais', '', '');
 
 -- --------------------------------------------------------
 
@@ -79,12 +61,20 @@ INSERT INTO `competences` (`ID`, `ID_etu`, `langues1`, `langues2`, `langues3`) V
 --
 
 CREATE TABLE `contact` (
-  `id_info` int(10) NOT NULL,
-  `adresse` varchar(10) NOT NULL,
+  `id` int(11) NOT NULL,
+  `age` int(10) NOT NULL,
+  `adresse` varchar(100) NOT NULL,
   `fixe` varchar(10) DEFAULT NULL,
   `portable` varchar(10) DEFAULT NULL,
   `ID_etu` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `contact`
+--
+
+INSERT INTO `contact` (`id`, `age`, `adresse`, `fixe`, `portable`, `ID_etu`) VALUES
+(0, 25, '15, rue hannah arendt 37200 Tours', '', '0675919856', 22);
 
 -- --------------------------------------------------------
 
@@ -103,16 +93,7 @@ CREATE TABLE `divers` (
 --
 
 INSERT INTO `divers` (`ID`, `ID_etu`, `divers`) VALUES
-(86, 8, 'efzfzef'),
-(87, 9, 'alc zclzeknc ezcezknc ezlcneapaez clen'),
-(88, 10, 'dandlka,dapoja,d padn al'),
-(100, 11, 'ldknad mpkanzd amkna apdnp'),
-(101, 13, 'fzfzefzfzfzfzfzefc e efd cze aé as '),
-(102, 14, 'd,anz z,nd azdnk, dan, a,n ,n '),
-(108, 15, ''),
-(109, 16, ''),
-(116, 17, 'kf &quot;kfn&quot;'),
-(117, 18, '');
+(123, 22, '');
 
 -- --------------------------------------------------------
 
@@ -134,17 +115,7 @@ CREATE TABLE `etudiant` (
 --
 
 INSERT INTO `etudiant` (`ID_etu`, `nom`, `prenom`, `mail`, `mdp`, `today`) VALUES
-(8, 'ducluzeaud', 'david', 'ducluzeaud.david@orange.fr', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '2017-03-02 02:33:21'),
-(9, 'toto', 'toto', 'tot@tot.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '2017-03-02 12:28:08'),
-(10, 'dupont', 'jean', 'jean.dupont@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '2017-03-03 12:23:31'),
-(11, 'ducluzeaud', 'colette', 'c.duc@duc.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '2017-03-03 12:44:03'),
-(12, 'tata', 'tata', 'tat@tat.om', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '2017-03-03 13:23:45'),
-(13, 'jean-marie', 'Ducluzeaud', 'jd@jd.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '2017-03-03 20:31:50'),
-(14, '  edez ', 'dl adln', 'a@a.a', 'd656370089fedbd4313c67bfdc24151fb7c0fe8b', '2017-03-03 20:38:05'),
-(15, 'fasquelle', 'daniel', 'd.fas@fas.com', '9d5c7d1e3663a680b1f8129cb05777c43391ec4d', '2017-03-03 20:59:40'),
-(16, 'estelle ', 'fasquelle', 'estelle.fasquelle@free.fr', '146dfd191464517a17306f915a8c6c11a01ecb75', '2017-03-03 21:03:51'),
-(17, 'fasquelle', 'fruit', 'fruit@fruit.com', '883d2500f8756e5985b2d676a470d0f2206208fc', '2017-03-03 21:51:37'),
-(18, 'dd', 'dd', 'dd@dd.com', '00c2e86d4d85eef415364b91eae9a6371fc48184', '2017-03-04 00:10:23');
+(22, 'ducluzeaud', 'david', 'ducluzeaud.david@etu.univ-tours.fr', '172e402b77059b0127f88a6752c85f63e9aa4d36', '2017-03-04 18:52:48');
 
 -- --------------------------------------------------------
 
@@ -168,16 +139,7 @@ CREATE TABLE `experiences` (
 --
 
 INSERT INTO `experiences` (`ID`, `ID_etu`, `annee_xp1`, `description1`, `annee_xp2`, `description2`, `annee_xp3`, `description3`) VALUES
-(8, 8, 2016, 'INSERM', 2000, '', 2000, ''),
-(9, 9, 0000, 'fzefzefz zfzc', 2000, '', 2000, ''),
-(10, 10, 2012, 'dnczdcnlzncnz', 2000, '', 2000, ''),
-(18, 11, 0000, 'caazxc', 0000, '', 0000, ''),
-(19, 13, 1970, 'knznzkne ezjnf zjenf  nez zj j', 0000, '', 0000, ''),
-(20, 14, 2000, 'kzenk kebzke kb k nz kf', 0000, '', 0000, ''),
-(25, 15, 0000, 'gzelgnlrn ', 0000, '', 0000, ''),
-(26, 16, 2016, 'assistante social', 0000, '', 0000, ''),
-(32, 17, 0000, 'fj&quot;nfkj&quot;nf', 0000, '', 0000, ''),
-(33, 18, 0000, 'dkakdnad', 0000, '', 0000, '');
+(38, 22, 2016, 'Stage d\'une durée de 2 mois au sein d\'une équipe INSERM au CEPR à tours', 0000, '', 0000, '');
 
 -- --------------------------------------------------------
 
@@ -210,31 +172,7 @@ CREATE TABLE `formations` (
 --
 
 INSERT INTO `formations` (`ID`, `ID_etu`, `annee_diplome1`, `intitule_formation1`, `universite1`, `mention1`, `description_form1`, `annee_diplome2`, `intitule_formation2`, `universite2`, `mention2`, `description_form2`, `annee_diplome3`, `intitule_formation3`, `universite3`, `mention3`, `description_form3`) VALUES
-(17, 8, 2016, 'Master biologie', 'tours', 'Excellent', 'fzfzfzefz', 2000, '', '', '', '', 2000, '', '', '', ''),
-(18, 9, 0000, 'Biologie', 'tours', 'Assez-Bien', 'znzenclzeknflzenmlznl llfnz', 2000, '', '', '', '', 2000, '', '', '', ''),
-(19, 10, 1945, 'armee', 'dadas', '', 'azazacacaa zda', 0000, '', '', '', '', 0000, '', '', '', ''),
-(24, 11, 2012, 'psychologie', 'axa', '', 'czdczcaax', 0000, '', '', '', '', 0000, '', '', '', ''),
-(25, 13, 1955, 'médecine', 'poitiers', 'Excellent', 'znfkznf nzadnoadn aiad aikn', 0000, '', '', '', '', 0000, '', '', '', ''),
-(26, 14, 1980, 'knzfbnz', 'kzdbakd', '', 'dkadaknd', 0000, '', '', '', '', 0000, '', '', '', ''),
-(29, 15, 0000, 'gel gre', 'g zg&quot;g&quot; ', '', 'gg g&quot; gez redf ', 0000, '', '', '', '', 0000, '', '', '', ''),
-(30, 16, 1985, 'bac', 'chartres', '', 'nndand &amp;jln &amp;nelnj ', 0000, '', '', '', '', 0000, '', '', '', ''),
-(31, 17, 0000, 'czkcn,', 'ecj,nze', '', 'jncznczjenc', 0000, '', '', '', '', 0000, '', '', '', ''),
-(32, 18, 0000, 'cnkzcnzk', 'nczkcnz', '', 'jczlcnzc oj ', 0000, '', '', '', '', 0000, '', '', '', '');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `informations`
---
-
-CREATE TABLE `informations` (
-  `id_info` int(10) NOT NULL,
-  `age` int(11) NOT NULL,
-  `adresse` varchar(250) NOT NULL,
-  `telephone_fixe` varchar(10) DEFAULT NULL,
-  `telephone_portable` varchar(10) DEFAULT NULL,
-  `ID_etu` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+(37, 22, 2016, 'Master biologie', 'Tours', '', 'Master 1 biologie santé', 0000, '', '', '', '', 0000, '', '', '', '');
 
 --
 -- Index pour les tables exportées
@@ -259,7 +197,7 @@ ALTER TABLE `competences`
 -- Index pour la table `contact`
 --
 ALTER TABLE `contact`
-  ADD PRIMARY KEY (`id_info`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `ID_etu` (`ID_etu`);
 
 --
@@ -291,12 +229,6 @@ ALTER TABLE `formations`
   ADD UNIQUE KEY `ID_etu` (`ID_etu`);
 
 --
--- Index pour la table `informations`
---
-ALTER TABLE `informations`
-  ADD PRIMARY KEY (`id_info`);
-
---
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -304,37 +236,32 @@ ALTER TABLE `informations`
 -- AUTO_INCREMENT pour la table `associatif`
 --
 ALTER TABLE `associatif`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
 --
 -- AUTO_INCREMENT pour la table `competences`
 --
 ALTER TABLE `competences`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
---
--- AUTO_INCREMENT pour la table `contact`
---
-ALTER TABLE `contact`
-  MODIFY `id_info` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 --
 -- AUTO_INCREMENT pour la table `divers`
 --
 ALTER TABLE `divers`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=126;
 --
 -- AUTO_INCREMENT pour la table `etudiant`
 --
 ALTER TABLE `etudiant`
-  MODIFY `ID_etu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `ID_etu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT pour la table `experiences`
 --
 ALTER TABLE `experiences`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 --
 -- AUTO_INCREMENT pour la table `formations`
 --
 ALTER TABLE `formations`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 --
 -- Contraintes pour les tables exportées
 --
@@ -350,6 +277,12 @@ ALTER TABLE `associatif`
 --
 ALTER TABLE `competences`
   ADD CONSTRAINT `competences_ibfk_1` FOREIGN KEY (`ID_etu`) REFERENCES `etudiant` (`ID_etu`);
+
+--
+-- Contraintes pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`ID_etu`) REFERENCES `etudiant` (`ID_etu`);
 
 --
 -- Contraintes pour la table `divers`
--- isset.php
+++ isset.php
@@ -53,67 +53,60 @@ if (isset ($_POST['valider'])){
   //insertion dans la table contact
   if (!empty($_POST['age']) and !empty($_POST['adresse'])){
     if ($age < 120) {
-    $fixe = strlen($fixe);
-    if ($fixe == 10) {
-      $portable = strlen($portable);
-      if ($portable == 10) {
-        $insertmbr = $bdd->prepare("INSERT INTO contact(age, adresse, fixe, portable, ID_etu) VALUES (?,?,?,?,?)");
-        $insertmbr->execute(array($age,$adresse,$fixe,$portable,$ID_etu));
-            header('Location: profil.php?ID_etu='.$_SESSION['ID_etu']);
+      $fixe = strlen($fixe);
+      if ($fixe <= 10) {
+        $portable = strlen($portable);
+        if ($portable <= 10) {
+          if (!empty($_POST['langue1'])){
+            if (!empty($_POST['intitule_formation1']) and !empty($_POST['description_form1']) and !empty($_POST['universite1'] )) {
+              if (!empty($_POST['annee_xp1']) and !empty($_POST['description_xp1'])) {
+
+                // insertion dans la table compétence
+                $insertmbr = $bdd->prepare("INSERT INTO contact(age, adresse, fixe, portable, ID_etu) VALUES (?,?,?,?,?)");
+                $insertmbr->execute(array($age,$adresse,$fixe,$portable,$ID_etu));
+
+                // insertion dans la table compétence
+                $insertmbr = $bdd->prepare("INSERT INTO competences(ID_etu,langues1,langues2,langues3) VALUES (?,?,?,?)");
+                $insertmbr->execute(array($ID_etu,$langue1,$langue2,$langue3));
+
+                // insertion dans la table expérience
+                $insertmbr = $bdd->prepare("INSERT INTO experiences(ID_etu, annee_xp1,description1,annee_xp2,description2,annee_xp3, description3 ) VALUES (?,?,?,?,?,?,?)");
+                $insertmbr->execute(array($ID_etu, $annee_xp1,$description_xp1,$annee_xp2,$description_xp2,$annee_xp3,$description_xp3));
+
+                $insertmbr = $bdd->prepare("INSERT INTO formations(ID_etu, annee_diplome1,intitule_formation1,universite1,mention1,description_form1,annee_diplome2,intitule_formation2,universite2,mention2,description_form2,annee_diplome3,intitule_formation3,universite3,mention3,description_form3 ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+                $insertmbr->execute(array($ID_etu, $annee_diplome1,$intitule_formation1,$universite1,$mention1,$description_form1,$annee_diplome2,$intitule_formation2,$universite2,$mention2,$description_form2,$annee_diplome3,$intitule_formation3,$universite3,$mention3,$description_form3));
+
+                // insertion dans la table associative
+                $insertmbr = $bdd->prepare("INSERT INTO associatif(ID_etu, association) VALUES (?,?)");
+                $insertmbr->execute(array($ID_etu, $association));
+
+                //insertion dans la table divers
+                $insertmbr = $bdd->prepare("INSERT INTO divers(ID_etu, divers) VALUES (?,?)");
+                $insertmbr->execute(array($ID_etu,$divers));
+
+                $erreur = "Votre CV a bien été créé ! <a class=\"centrer\">Retournez vers votre profil !</a>";
+
+              } else {
+                $erreur = "Veuillez renseigner tous les champs d'experience 1";
+              }
+
+            } else {
+              $erreur = 'Veuillez renseigner tous les champs de la formation 1.';
+            }
+          } else {
+            $erreur = "Veuilez rentrer valeur dans le champ langue 1";
+          }
+        } else {
+          $erreur = "Votre numéro de portable ne doit pas dépasser 10 chiffres !";
+        }
       } else {
-        $erreur = "Votre numéro de portable ne doit pas dépasser 10 chiffres !";
+        $erreur = "Votre numéro ne doit pas dépasser 10 chiffres !";
       }
-    } else {
-      $erreur = "Votre numéro ne doit pas dépasser 10 chiffres !";
+    }else {
+      $erreur = "Votre age ne peut dépasser 120 ans !";
     }
   }else {
-    $erreur = "Votre age ne peut dépasser 120 ans !";
+    $erreur = "Veuilez rentrer valeur dans le champ age et/ou adresse";
   }
-}else {
-  $erreur = "Veuilez rentrer valeur dans le champ age et/ou adresse";
-  }
-  // insertion dans la table competences
-
-  if (!empty($_POST['langue1'])){
-    $insertmbr = $bdd->prepare("INSERT INTO competences(ID_etu,langues1,langues2,langues3) VALUES (?,?,?,?)");
-    $insertmbr->execute(array($ID_etu,$langue1,$langue2,$langue3));
-  } else {
-    $erreur = "Veuilez rentrer valeur dans le champ langue 1";
-  }
-
-
-  // insertion dans la table formations
-
-
-  if (!empty($_POST['description_form1']) and !empty($_POST['universite1'] )) {
-
-    $insertmbr = $bdd->prepare("INSERT INTO formations(ID_etu, annee_diplome1,intitule_formation1,universite1,mention1,description_form1,annee_diplome2,intitule_formation2,universite2,mention2,description_form2,annee_diplome3,intitule_formation3,universite3,mention3,description_form3 ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-    $insertmbr->execute(array($ID_etu, $annee_diplome1,$intitule_formation1,$universite1,$mention1,$description_form1,$annee_diplome2,$intitule_formation2,$universite2,$mention2,$description_form2,$annee_diplome3,$intitule_formation3,$universite3,$mention3,$description_form3));
-        header('Location: profil.php?ID_etu='.$_SESSION['ID_etu']);
-  } else {
-    $erreur = 'Veuillez renseigner tous les champs de la formation 1.';
-  }
-
-
-  // insertion dans la table expérience
-  if (!empty($_POST['annee_xp1']) and !empty($_POST['description_xp1'])) {
-
-    $insertmbr = $bdd->prepare("INSERT INTO experiences(ID_etu, annee_xp1,description1,annee_xp2,description2,annee_xp3, description3 ) VALUES (?,?,?,?,?,?,?)");
-    $insertmbr->execute(array($ID_etu, $annee_xp1,$description_xp1,$annee_xp2,$description_xp2,$annee_xp3,$description_xp3));
-        header('Location: profil.php?ID_etu='.$_SESSION['ID_etu']);
-  } else {
-    $erreur = "Veuillez renseigner tous les champs d'experience 1";
-  }
-
-
-  // insertion dans la table associative
-  $insertmbr = $bdd->prepare("INSERT INTO associatif(ID_etu, association) VALUES (?,?)");
-  $insertmbr->execute(array($ID_etu, $association));
-
-  //insertion dans la table divers
-  $insertmbr = $bdd->prepare("INSERT INTO divers(ID_etu, divers) VALUES (?,?)");
-  $insertmbr->execute(array($ID_etu,$divers));
-
-
 }
 ?>
